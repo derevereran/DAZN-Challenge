96663d2c88dc1200fae98ed205399742cac29503
devran <atesdevran@gmail.com>
2021-09-14 17:22:08 +0300

    news

---
+++ DAZN-CASE/DAZN-CASE/Modules/News/Cells/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  DAZN-CASE
+//
+//  Created by Devran Ates on 14.09.2021.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
